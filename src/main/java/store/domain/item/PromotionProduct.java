daa5b7e7ad8af29e375c2d3ae3905ec6f4206249
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-22 13:51:04 +0900

    feat : 상품 객체 생성

---
+++ src/main/java/store/domain/item/PromotionProduct.java
@@ -0,0 +1,10 @@
+package store.domain.item;
+
+public class PromotionProduct extends Product {
+    private final Promotion promotion;
+
+    public PromotionProduct(String name, int price, int quantity, Promotion promotion) {
+        super(name, price, quantity);
+        this.promotion = promotion;
+    }
+}
