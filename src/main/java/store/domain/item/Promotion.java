aed7cd6064e4ff6c14185158eacae20609d58874
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-22 13:50:53 +0900

    feat : 프로모션 객체 생성

---
+++ src/main/java/store/domain/item/Promotion.java
@@ -0,0 +1,21 @@
+package store.domain.item;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+
+    public Promotion(String name, int buy, int get,
+                     LocalDateTime startDate, LocalDateTime endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+}
