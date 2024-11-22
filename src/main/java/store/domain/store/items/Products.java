98c4f28d09dc05c5641b9aebb049003a67eb669f
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-22 15:28:54 +0900

    fix : 프로모션 기간 외 상품 일반 상품으로

--- src/main/java/store/domain/store/items/Products.java
+++ src/main/java/store/domain/store/items/Products.java
@@ -1,13 +1,41 @@
 package store.domain.store.items;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import store.domain.store.item.Product;
+import store.domain.store.item.PromotionProduct;
 
 public class Products {
     private final List<Product> products;
 
     public Products(List<Product> products) {
         this.products = products;
+    }
+
+    public Product findProductByNameAndDate(String name, LocalDateTime nowDate) {
+        for (Product product : products) {
+            if (product.isSameName(name)) {
+                if (product instanceof PromotionProduct promotionProduct) {
+                    if (promotionProduct.isValidDate(nowDate)) {
+                        return promotionProduct;
+                    }
+                    continue;
+                }
+                return product;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 상품입니다. 다시 입력해 주세요.");
+    }
+
+    public boolean isEnoughStock(String name, int quantity) {
+        int totalStock = 0;
+
+        for (Product product : products) {
+            if (product.isSameName(name)) {
+                totalStock += product.getQuantity();
+            }
+        }
+        return totalStock >= quantity;
     }
 
     @Override
