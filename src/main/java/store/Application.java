4d953794dd5c55f851db2e662fe5ba3c6cbc16be
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-22 13:46:26 +0900

    feat : 메인 컨트롤러 Application에 연결

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.controller.ConvenienceController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ConvenienceController convenienceController = new ConvenienceController();
+        convenienceController.run();
     }
 }
---
+++ src/main/java/store/controller/ConvenienceController.java
@@ -0,0 +1,8 @@
+package store.controller;
+
+public class ConvenienceController {
+
+    public void run() {
+
+    }
+}
