c5323fc75226c43c848cf055db24f6765bb44b9c
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-22 14:08:31 +0900

    test :.md 파일 읽어서 Promotion 생성하기 테스트

---
+++ src/test/java/store/domain/PromotionReaderTest.java
@@ -0,0 +1,21 @@
+package store.domain;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import store.domain.item.Promotion;
+
+class PromotionReaderTest {
+
+    @Test
+    void 프로모션_상품_생성하기_확인() {
+        String path = "src/main/resources/promotions.md";
+
+        PromotionReader promotionReader = new PromotionReader();
+        List<Promotion> actual = promotionReader.read(path);
+
+        assertNotNull(actual);
+    }
+
+}
